fe595603b2bafb5c6b6025a354f2915516066ebd
CodeCodeChicken <1336445024@qq.com>
2022-07-13 17:48:14 +0800

    配置文件config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      // 配置别名
+      alias: {
+        assets: 'src/assets',
+        common: 'src/common',
+        components: 'src/components',
+        network: 'src/network',
+        views: 'src/views'
+        /*
+        不需要配置store和router
+        在所有组件下都可使用
+        this.$router
+        this.$store
+        获取到对象
+        */
+      }
+    }
+  }
+}
